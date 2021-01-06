ab3a0c2d1fbcfd0f92b69533375bfae3b21e6004
Mark Fernandez <marksaviofernandez@gmail.com>
2021-01-05 20:54:55 -0800

    Adding vec3 class and refactoring main function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <fstream>
 
-int main(){
+#include "vec3.h"
 
-    int nx = 200;
-    int ny = 100;
 
-    std::ofstream ofs;
-
+void check_version_cpp(){
 
     if (__cplusplus == 201703L) std::cout << "C++17\n";
     else if (__cplusplus == 201402L) std::cout << "C++14\n";
@@ -15,11 +12,23 @@ int main(){
     else if (__cplusplus == 199711L) std::cout << "C++98\n";
     else std::cout << "pre-standard C++\n";
 
+}
+
+
+void write_test_ppm(){
+
+    int nx = 200;
+    int ny = 100;
+
+
+    std::ofstream ofs;
+
+
     try {
 
-    	ofs.open("test.ppm", std::ios::binary);
+        ofs.open("test.ppm", std::ios::binary);
 
-    	ofs << "P3 \n" << nx << " " << ny << "\n255\n";
+        ofs << "P3 \n" << nx << " " << ny << "\n255\n";
 
         for(int j = ny - 1; j >= 0; j--) {
             for(int i = 0; i < nx; i++) {
@@ -44,6 +53,102 @@ int main(){
         ofs.close(); 
     } 
 
+}
+
+
+
+//In order to use do ./a.out > testimage.ppm.
+//WARN: Command line cant print out anything else to std::cout 
+void write_test_ppm_commandline() {
+
+    int nx = 200;
+    int ny = 100;
+
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for(int j  = ny - 1; j >= 0; j--){
+
+        for(int i = 0; i < nx; i++){
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+
+        }
+    }
+}
+
+void test_vec3(){
+
+    vec3 col(10.0, 10.0, 10.0);
+    std::cout << "Testing " <<  col.r() << std::endl;
+
+    std::cout << "Testing dot product " << dot(col, col) << std::endl;
+
+    vec3 mul = col * col;
+    vec3 add = col + col;
+    vec3 sub = col - col;
+    vec3 div = col / col;
+
+    std::cout << "Testing multiplication " << mul.x() << std::endl;
+
+    std::cout << "Testing addition " << add.x() << std::endl;
+
+    std::cout << "Testing subtraction " << sub.x() << std::endl;
+
+    std::cout << "Testing division " << div.x() << std::endl;
+
+    vec3 v1(1.0, 2.0, 3.0);
+    vec3 v2(10.0, 2.0, 4.0);
+
+    vec3 vcross = cross(v1, v2);
+
+    std::cout << "Testing cross product " <<  vcross.x() << " " << vcross.y() << " " << vcross.z() << " " << std::endl;
+
+    v1 += v2;
+    std::cout << "Testing += " <<  v1.x() << " " << v1.y() << " " << v1.z() << " " << std::endl;
+
+    v1 -= v2;
+    std::cout << "Testing -= " <<  v1.x() << " " << v1.y() << " " << v1.z() << " " << std::endl;
+
+
+    v1 *= v2;
+    std::cout << "Testing *= " <<  v1.x() << " " << v1.y() << " " << v1.z() << " " << std::endl;
+
+
+    v1 /= v2;
+    std::cout << "Testing /= " <<  v1.x() << " " << v1.y() << " " << v1.z() << " " << std::endl;
+
+    v1 *= 10;
+    std::cout << "Testing *= " <<  v1.x() << " " << v1.y() << " " << v1.z() << " " << std::endl;
+
+    v1 /= 10;
+    std::cout << "Testing *= " <<  v1.x() << " " << v1.y() << " " << v1.z() << " " << std::endl;
+
+
+    float len = v1.length();
+    std::cout << " Length " <<  len << std::endl;
+
+
+    float sq_len = v1.squared_length();
+    std::cout << "Squared length " <<  sq_len << std::endl;
+
+    std::cout <<  "Testing vector output " << v1 << std::endl;
+
+}
+
+
+int main(){
+
+    //check_version_cpp();
+
+    //write_test_ppm();
+
+    test_vec3();
+
+    return 0;
     
  
 
---
+++ vec3.h
@@ -0,0 +1,76 @@
+#ifndef VEC3
+#define VEC3
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+class vec3
+{
+
+public:
+    vec3() {
+         e[0] = 0.0;
+         e[1] = 0.0;
+         e[2] = 0.0;
+
+    }
+    
+    vec3(float e0, float e1, float e2) 
+    {
+        e[0] = e0;
+        e[1] = e1;
+        e[2] = e2;
+    }
+    
+
+    inline float x() const {return e[0];}
+    inline float y() const {return e[1];}
+    inline float z() const {return e[2];}
+    inline float r() const {return e[0];}
+    inline float g() const {return e[1];}
+    inline float b() const {return e[2];}
+
+    vec3& operator+=(const vec3 &v);
+    vec3& operator-=(const vec3 &v);
+    vec3& operator*=(const vec3 &v);
+    vec3& operator/=(const vec3 &v);
+    vec3& operator*=(const float t);
+    vec3& operator/=(const float t);
+
+
+    inline float length() const {
+        return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+    }
+
+    inline float squared_length() const {
+        return (e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+    }
+
+
+private:
+
+    float e[3];
+
+};
+
+
+std::ostream& operator<<(std::ostream &os, const vec3 &v);
+
+
+float dot(const vec3 &v1, const vec3 &v2);
+
+vec3 cross(const vec3 &v1, const vec3 &v2);
+
+vec3 operator*(const vec3 &v1, const vec3 &v2);
+
+vec3 operator+(const vec3 &v1, const vec3 &v2);
+
+vec3 operator-(const vec3 &v1, const vec3 &v2);
+
+vec3 operator/(const vec3 &v1, const vec3 &v2);
+
+
+
+#endif
